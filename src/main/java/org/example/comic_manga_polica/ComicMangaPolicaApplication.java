99f6aee9d29876bebdd7c5e455bfe1867ba79e9f
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2026-06-13 16:54:14 +0200

    inicijalni

---
+++ src/main/java/org/example/comic_manga_polica/ComicMangaPolicaApplication.java
@@ -0,0 +1,13 @@
+package org.example.comic_manga_polica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicMangaPolicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicMangaPolicaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/comic_manga_polica/ComicMangaPolicaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.comic_manga_polica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicMangaPolicaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
