09b48ed3302df374e7d960446ba19a84673e0144
LukaRitosa <165939171+LukaRitosa@users.noreply.github.com>
2026-06-15 15:06:33 +0200

    commit

--- src/main/java/org/example/comic_manga_polica/service/BookShelfService.java
+++ src/main/java/org/example/comic_manga_polica/service/BookShelfService.java
@@ -1,4 +1,14 @@
 package org.example.comic_manga_polica.service;
 
+import org.example.comic_manga_polica.dto.BookShelfRequest;
+import org.example.comic_manga_polica.entity.BookShelf;
+
+import java.util.List;
+
 public interface BookShelfService {
+    List<BookShelf> FindAll();
+    BookShelf findById(Long Id);
+    BookShelf create(BookShelfRequest req);
+    BookShelf update(BookShelfRequest req);
+    Void delete(Long comicId);
 }
--- src/main/java/org/example/comic_manga_polica/service/ComicService.java
+++ src/main/java/org/example/comic_manga_polica/service/ComicService.java
@@ -1,4 +1,16 @@
 package org.example.comic_manga_polica.service;
 
+
+import org.example.comic_manga_polica.dto.ComicRequest;
+import org.example.comic_manga_polica.entity.Comic;
+
+import java.util.List;
+
 public interface ComicService {
+    List<Comic> FindAll();
+    Comic findById(Long Id);
+    Comic create(ComicRequest req);
+    Comic update(ComicRequest req);
+    Void delete(Long comicId);
+    Comic findByShelfId(Long shelfId);
 }
